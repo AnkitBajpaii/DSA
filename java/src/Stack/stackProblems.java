edb6ce46ad28794a56c77beafe0e900b13da0661
Ankit Bajpai <ankitbajpaib070@gmail.com>
2022-04-17 16:20:09 +0530

    Added problems on stacks

--- java/src/Stack/stackProblems.java
+++ java/src/Stack/stackProblems.java
@@ -451,6 +451,71 @@ public class stackProblems {
     Elements for which there is no previous smaller element, output -1 for those elements
     i/p:  4 2  5 10 8 2
     o/p: -1 -1 2 5 5 -1
+
+    Given an array A, find the nearest smaller element G[i] for every element A[i] in the array such that the element has an index smaller than i.
+
+    More formally,
+
+    G[i] for an element A[i] = an element A[j] such that
+
+    j is maximum possible AND
+
+    j < i AND
+
+    A[j] < A[i]
+
+    Elements for which no smaller element exist, consider the next smaller element as -1.
+
+
+
+    Problem Constraints
+    1 <= |A| <= 100000
+
+    -109 <= A[i] <= 109
+
+
+
+    Input Format
+    The only argument given is integer array A.
+
+
+
+    Output Format
+    Return the integar array G such that G[i] contains the nearest smaller number than A[i]. If no such element occurs G[i] should be -1.
+
+
+
+    Example Input
+    Input 1:
+
+    A = [4, 5, 2, 10, 8]
+    Input 2:
+
+    A = [3, 2, 1]
+
+
+    Example Output
+    Output 1:
+
+    [-1, 4, -1, 2, 2]
+    Output 2:
+
+    [-1, -1, -1]
+
+
+    Example Explanation
+    Explanation 1:
+
+    index 1: No element less than 4 in left of 4, G[1] = -1
+    index 2: A[1] is only element less than A[2], G[2] = A[1]
+    index 3: No element less than 2 in left of 2, G[3] = -1
+    index 4: A[3] is nearest element which is less than A[4], G[4] = A[3]
+    index 4: A[3] is nearest element which is less than A[5], G[5] = A[3]
+    Explanation 2:
+
+    index 1: No element less than 3 in left of 3, G[1] = -1
+    index 2: No element less than 2 in left of 2, G[2] = -1
+    index 3: No element less than 1 in left of 1, G[3] = -1
     */
     public int[] nearestSmallerElementOnLeftSide(int[] A) {
         int n = A.length;
@@ -562,20 +627,15 @@ public class stackProblems {
         int ans = 0;
 
         for (int i = 0; i < A.length; i++) {
-            
-            int s = ngl[i]; // index of nearest greater on left
-            int e = ngr[i]; // index of nearest greater on right
 
             // no of sub arrays where A[i] is max = count no of sub arrays between s to e (both index excluded) where A[i] is present
-            // = (i - (s+1)+1) * ((e -1) - i + 1) ==> (i - s) * (e - i)
+            // = (i - ( index of nearest greater on left +1)+1) * ((index of nearest greater on right -1) - i + 1) 
+            //==> (i - index of nearest greater on left) * (index of nearest greater on right - i)
 
-            ans += A[i] * (i - s) * (e - i);
+            ans += A[i] * (i - ngl[i]) * (ngr[i] - i);
 
             // no of sub arrays where A[i] is min
-            s = nsl[i];
-            e = nsr[i];
-
-            ans -= A[i] * (i - s) * (e - i);
+            ans -= A[i] * (i - nsl[i]) * (nsr[i] - i);
         }
 
         return ans;
