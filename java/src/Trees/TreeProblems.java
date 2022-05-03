76d879533a4890bc7312272b2ef0723897e24d95
Ankit Bajpai <ankitbajpaib070@gmail.com>
2022-05-03 15:20:23 +0530

    Added problems on Trees

--- java/src/Trees/TreeProblems.java
+++ java/src/Trees/TreeProblems.java
@@ -1563,4 +1563,110 @@ Explanation 2:
             return info.ans;
         }
     }
+
+    /* Boundary Traversal Of Binary Tree
+    Given a binary tree. Given a binary tree, return the values of its boundary in anti-clockwise direction starting from the root. Boundary includes left boundary, leaves, and right boundary in order without duplicate nodes.
+
+Left boundary is defined as the path from the root to the left-most node. Right boundary is defined as the path from the root to the right-most node. If the root doesn't have left subtree or right subtree, then the root itself is left boundary or right boundary. Note this definition only applies to the input binary tree, and not applies to any subtrees.
+
+The left-most node is defined as a leaf node you could reach when you always firstly travel to the left subtree if exists. If not, travel to the right subtree. Repeat until you reach a leaf node.
+
+The right-most node is also defined by the same way with left and right exchanged.
+
+Return an array of integers denoting the boundary values of tree in anti-clockwise order.
+
+For Example
+
+Input 1:
+               _____1_____
+              /           \
+             2             3
+            / \            / 
+           4   5          6   
+              / \        / \
+             7   8      9  10  
+Output 1:
+    [1, 2, 4, 7, 8, 9, 10, 6, 3]
+    Explanation 1:
+        The left boundary are node 1,2,4. (4 is the left-most node according to definition)
+        The leaves are node 4,7,8,9,10.
+        The right boundary are node 1,3,6,10. (10 is the right-most node).
+        So order them in anti-clockwise without duplicate nodes we have [1,2,4,7,8,9,10,6,3].
+
+Input 2:
+                1
+               / \
+              2   3
+             / \  / \
+            4   5 6  7
+Output 2:
+     [1, 2, 4, 5, 6, 7, 3] 
+    */
+    void GetLeftMost(TreeNode root, ArrayList<Integer> al) {
+        TreeNode curr = root;
+
+        while (curr != null) {
+            al.add(curr.val);
+
+            if (curr.left != null) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+    }
+
+    void GetRightMost(TreeNode root, ArrayList<Integer> al) {
+        TreeNode curr = root;
+        Stack<Integer> st = new Stack<Integer>();
+
+        while (curr != null) {
+            st.push(curr.val);
+
+            if (curr.right != null) {
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+
+        while (!st.isEmpty()) {
+            al.add(st.pop());
+        }
+    }
+
+    void GetLeafs(TreeNode root, ArrayList<Integer> al) {
+        if (root == null)
+            return;
+
+        if (root.left == null && root.right == null) {
+            al.add(root.val);
+            return;
+        }
+
+        GetLeafs(root.left, al);
+
+        GetLeafs(root.right, al);
+    }
+
+    public ArrayList<Integer> solve(TreeNode A) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+
+        GetLeftMost(A, al);
+        GetLeafs(A, al);
+        GetRightMost(A, al);
+
+        HashSet<Integer> set = new HashSet<Integer>();
+        ArrayList<Integer> res = new ArrayList<Integer>();
+
+        for (int x : al) {
+            if (set.contains(x))
+                continue;
+
+            res.add(x);
+            set.add(x);
+        }
+
+        return res;
+    }
 }
