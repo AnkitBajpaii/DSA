1734dc30c30a410c2126447adb0c1107f7a838ad
Ankit Bajpai <ankitbajpaib070@gmail.com>
2022-05-21 01:06:00 +0530

    Added problems on BackTrackking

--- java/src/Backtrackking/BackTrackkingProblems.java
+++ java/src/Backtrackking/BackTrackkingProblems.java
@@ -1,21 +1,20 @@
 package Backtrackking;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class BackTrackkingProblems {
 
-    /*
-     * Print all N digit numbers, starting with [1, 2]
+    /* Generate all N digit numbers, using digits only 1 & 2
      */
-    public void PrintNDigitNumbers(int N) {
+    public void GenerateNDigitNumbersUsing1And2(int N) {
         int[] currList = new int[N];
 
         ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
 
-        PrintNDigitNumbersUtil(N, 0, currList, output);
+        GenerateNDigitNumbersUsing1And2Util(N, 0, currList, output);
     }
 
-    private void PrintNDigitNumbersUtil(int N, int index, int[] currList, ArrayList<ArrayList<Integer>> output) {
+    private void GenerateNDigitNumbersUsing1And2Util(int N, int index, int[] currList, ArrayList<ArrayList<Integer>> output) {
         if (index == N) {
             ArrayList<Integer> al = new ArrayList<>();
 
@@ -27,35 +26,135 @@ public class BackTrackkingProblems {
         }
 
         currList[index] = 1;
-        PrintNDigitNumbersUtil(N, index + 1, currList, output);
+        GenerateNDigitNumbersUsing1And2Util(N, index + 1, currList, output);
 
         currList[index] = 2;
-        PrintNDigitNumbersUtil(N, index + 1, currList, output);
+        GenerateNDigitNumbersUsing1And2Util(N, index + 1, currList, output);
     }
 
-    /* Given a set of integers, find all its subsets. 
+    /* SubSet
+    Problem Description
+Given a set of distinct integers A, return all possible subsets.
+
+NOTE:
+
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+Also, the subsets should be sorted in ascending ( lexicographic ) order.
+The list is not necessarily sorted.
+
+
+Problem Constraints
+1 <= |A| <= 16
+INTMIN <= A[i] <= INTMAX
+
+
+Input Format
+First and only argument of input contains a single integer array A.
+
+
+
+Output Format
+Return a vector of vectors denoting the answer.
+
+
+
+Example Input
+Input 1:
+
+A = [1]
+Input 2:
+
+A = [1, 2, 3]
+
+
+Example Output
+Output 1:
+
+[
+    []
+    [1]
+]
+Output 2:
+
+[
+ []
+ [1]
+ [1, 2]
+ [1, 2, 3]
+ [1, 3]
+ [2]
+ [2, 3]
+ [3]
+]
+
+
+Example Explanation
+Explanation 1:
+
+ You can see that these are all possible subsets.
+Explanation 2:
+
+You can see that these are all possible subsets.
     */
-    public void GenerateAllSubsets(int[] A)
+    void findSubSet(ArrayList<Integer> A, int index, ArrayList<Integer> output, ArrayList<ArrayList<Integer>> answer)
     {
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-
-        GenerateAllSubsetsUtil(A, 0, new ArrayList<Integer>(), ans);
-    }
-
-    private void GenerateAllSubsetsUtil(int[] A, int index, ArrayList<Integer> output, ArrayList<ArrayList<Integer>> ans){
-
-        if(index == A.length)
+        if(index == A.size())
         {
-            ans.add(output);
+            answer.add(output);
             return;
         }
 
-         // Not Including Value which is at Index
-        GenerateAllSubsetsUtil(A, index + 1, new ArrayList<Integer>(output), ans);
+        findSubSet(A, index+1, new ArrayList<Integer>(output), answer);
 
-         // Including Value which is at Index
-        output.add(A[index]);
-        GenerateAllSubsetsUtil(A, index + 1, new ArrayList<Integer>(output), ans);
+        output.add(A.get(index));
+
+        findSubSet(A, index+1, new ArrayList<Integer>(output), answer);
+    }
+
+    public ArrayList<ArrayList<Integer>> subsets(ArrayList<Integer> A) {
+
+        Collections.sort(A);
+        ArrayList<ArrayList<Integer>> answer = new ArrayList<ArrayList<Integer>>();
+
+        findSubSet(A, 0, new ArrayList<Integer>(), answer);
+
+        Collections.sort(answer, new Comparator<ArrayList<Integer>>() {
+
+            @Override
+            public int compare(ArrayList<Integer> al1, ArrayList<Integer> al2)
+            {
+                if(al1.size() == 0) return -1;
+
+                if(al2.size() == 0) return 1;
+                
+                int i=0;
+
+                while(i<al1.size() && i<al2.size())
+                {
+                    if(al1.get(i) < al2.get(i))
+                    {
+                        return -1;
+                    }
+
+                    if(al1.get(i) > al2.get(i))
+                    {
+                        return 1;
+                    }
+
+                    i++;
+                }
+
+                if(al1.size() < al2.size()) return -1;
+
+                if(al1.size() > al2.size()) return 1;
+
+                return 0;
+            }
+
+        });
+
+        return answer;
     }
 
     /*
@@ -72,6 +171,82 @@ public class BackTrackkingProblems {
 
         return countSubSetWithGivenSumUtil(A, index + 1, sum)
                 + countSubSetWithGivenSumUtil(A, index + 1, sum - A[index]);
+    }
+
+    /* SIXLETS
+    Problem Description
+Given a array of integers A of size N and an integer B.
+
+Return number of non-empty subsequences of A of size B having sum <= 1000.
+
+
+
+Problem Constraints
+1 <= N <= 20
+
+1 <= A[i] <= 1000
+
+1 <= B <= N
+
+
+
+Input Format
+The first argument given is the integer array A.
+
+The second argument given is the integer B.
+
+
+
+Output Format
+Return number of subsequences of A of size B having sum <= 1000.
+
+
+
+Example Input
+Input 1:
+
+    A = [1, 2, 8]
+    B = 2
+Input 2:
+
+    A = [5, 17, 1000, 11]
+    B = 4
+
+
+Example Output
+Output 1:
+
+3
+Output 2:
+
+0
+
+
+Example Explanation
+Explanation 1:
+
+ {1, 2}, {1, 8}, {2, 8}
+Explanation 1:
+
+ No valid subsequence
+    */
+    int countSubsequenceOfGivenSizeWithSumLessThan1000Util(ArrayList<Integer> A, int index, int count, int sum) {
+        if (sum > 1000)
+            return 0;
+
+        if (count == 0)
+            return 1;
+
+        if (index == A.size())
+            return 0;
+
+        return countSubsequenceOfGivenSizeWithSumLessThan1000Util(A, index + 1, count, sum)
+                + countSubsequenceOfGivenSizeWithSumLessThan1000Util(A, index + 1, count - 1, sum + A.get(index));
+
+    }
+
+    public int countSubsequenceOfGivenSizeWithSumLessThan1000(ArrayList<Integer> A, int B) {
+        return countSubsequenceOfGivenSizeWithSumLessThan1000Util(A, 0, B, 0);
     }
 
     /* Rat In a Maze
