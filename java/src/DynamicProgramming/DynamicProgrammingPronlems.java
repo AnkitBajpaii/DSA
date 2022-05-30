dd293fe2b977b76b754bfa97c68156b22a6f78b5
Ankit Bajpai <ankitbajpaib070@gmail.com>
2022-05-30 16:22:58 +0530

    Added problems on DP

--- java/src/DynamicProgramming/DynamicProgrammingPronlems.java
+++ java/src/DynamicProgramming/DynamicProgrammingPronlems.java
@@ -345,5 +345,159 @@ Explanation 2:
             return countMinSquaresUtilTabulation(A);
         }
     }
+
+    /* Ways to Decode
+    Problem Description
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message denoted by string A containing digits, determine the total number of ways to decode it modulo 109 + 7.
+
+
+
+Problem Constraints
+1 <= length(A) <= 105
+
+
+
+Input Format
+The first and the only argument is a string A.
+
+
+
+Output Format
+Return an integer, representing the number of ways to decode the string modulo 109 + 7.
+
+
+
+Example Input
+Input 1:
+
+ A = "12"
+Input 2:
+
+ A = "8"
+
+
+Example Output
+Output 1:
+
+ 2
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+ Given encoded message "8", it could be decoded as only "H" (8).
+ The number of ways decoding "8" is 1.
+Explanation 2:
+
+ Given encoded message "12", it could be decoded as "AB" (1, 2) or "L" (12).
+ The number of ways decoding "12" is 2.
+    */
+    public long numDecodingsUtil(String A, int N) {
+
+        if (N == 0 || N == 1) {
+            return 1;
+        }
+
+        if (A.charAt(0) == '0') {
+            return 0;
+        }
+
+        long count = 0;
+
+        if (A.charAt(N - 1) > '0') {
+            count = numDecodingsUtil(A, N - 1);
+        }
+
+        if (A.charAt(N - 2) == '1' || (A.charAt(N - 2) == '2' && A.charAt(N - 1) < '7')) {
+            count += numDecodingsUtil(A, N - 2);
+        }
+
+        return count;
+    }
+
+    public long numDecodingsUtilMemoization(String A, int N, long[] dp) {
+
+        int mod = 1000 * 1000 * 1000 + 7;
+
+        if (dp[N] == -1) {
+            if (N == 0 || N == 1) {
+                dp[N] = 1;
+            } else if (A.charAt(0) == '0') {
+                dp[N] = 0;
+            } else {
+                long count = 0;
+
+                if (A.charAt(N - 1) > '0') {
+                    count = numDecodingsUtilMemoization(A, N - 1, dp);
+                }
+
+                if (A.charAt(N - 2) == '1' || (A.charAt(N - 2) == '2' && A.charAt(N - 1) < '7')) {
+                    count = (count + numDecodingsUtilMemoization(A, N - 2, dp)) % mod;
+                }
+
+                dp[N] = count;
+            }
+        }
+
+        return dp[N];
+    }
+
+    public int numDecodingsUtilTabulation(String A, int N) {
+        if (N == 0 || A.charAt(0) == '0')
+            return 0;
+
+        if (N == 1)
+            return 1;
+
+        int mod = 1000 * 1000 * 1000 + 7;
+
+        long[] dp = new long[N + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = 0;
+
+            if (A.charAt(i - 1) > '0') {
+                dp[i] = dp[i - 1];
+            }
+
+            if (A.charAt(i - 2) == '1' || (A.charAt(i - 2) == '2' && A.charAt(i - 1) < '7')) {
+                dp[i] = (dp[i] + dp[i - 2]) % mod;
+            }
+        }
+
+        return (int) (dp[N] % mod);
+    }
+
+    public int numDecodings(String A) {
+
+        int N = A.length();
+
+        return numDecodingsUtilTabulation(A, N);
+
+        // int mod = 1000 * 1000 * 1000 + 7;
+        // if (N == 0 || A.charAt(0) == '0')
+        //     return 0;
+
+        // long[] dp = new long[N+1];
+        // for(int i=0; i<=N; i++)
+        // {
+        //     dp[i] = -1;
+        // }
+
+        // long ans = numDecodingsUtilMemoization(A, N, dp);
+
+        // return (int) (ans % mod);
+    }
 }
 
